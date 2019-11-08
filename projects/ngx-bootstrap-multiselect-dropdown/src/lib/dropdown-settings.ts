f4e15ba121dffb81541295f8f468c467d880732d
Jon Hancox <JHancox@ipipeline.com>
2019-11-08 14:00:56 +0000

    Add functionality for a custom message instead of 'item' selected

--- projects/ngx-bootstrap-multiselect-dropdown/src/lib/dropdown-settings.ts
+++ projects/ngx-bootstrap-multiselect-dropdown/src/lib/dropdown-settings.ts
@@ -14,15 +14,17 @@ export class DropdownSettings {
     showDeselectAllBtn: boolean = null;
     showSelectAllBtn: boolean = null;
     btnClasses: string = null;
+    itemSingluarText: string = 'item';
+    itemPluralText: string = 'items';
 
     constructor(settings?: any) {
         if(!settings) return;
-        this.noneSelectedBtnText = settings.noneSelectedBtnText || this.noneSelectedBtnText ;
+        this.noneSelectedBtnText = settings.noneSelectedBtnText || this.noneSelectedBtnText;
         this.dataIdProperty = settings.dataIdProperty || this.dataIdProperty;
         this.dataNameProperty = settings.dataNameProperty || this.dataNameProperty;
         this.deselectAllBtnText = settings.deselectAllBtnText || this.deselectAllBtnText;
         this.enableFilter = settings.enableFilter;
-        this.selectAllBtnText = settings.selectAllBtnText || this.selectAllBtnText;8
+        this.selectAllBtnText = settings.selectAllBtnText || this.selectAllBtnText;
         this.dropdownClasses = settings.dropdownClasses;
         this.headerText = settings.headerText;
         this.selectionLimit = settings.selectionLimit;
@@ -31,5 +33,7 @@ export class DropdownSettings {
         this.btnWidth = settings.btnWidth || this.btnWidth; 
         this.dropdownHeight = settings.dropdownHeight || this.dropdownHeight;
         this.btnClasses = settings.btnClasses;
+        this.itemSingluarText = settings.itemSingularText || this.itemSingluarText;
+        this.itemPluralText = settings.itemPluralText || this.itemPluralText;
     }
 }
--- projects/ngx-bootstrap-multiselect-dropdown/src/lib/ngx-bootstrap-multiselect-dropdown.component.ts
+++ projects/ngx-bootstrap-multiselect-dropdown/src/lib/ngx-bootstrap-multiselect-dropdown.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit, forwardRef, Input, Output, EventEmitter, HostListene
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import { DropdownSettings } from './dropdown-settings';
 export const DROPDOWN_CONTROL_VALUE_ACCESSOR: any = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => NgxBootstrapMultiselectDropdownComponent),
-  multi: true
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => NgxBootstrapMultiselectDropdownComponent),
+    multi: true
 };
 @Component({
-  selector: 'ngx-bootstrap-multiselect',
-  template: `
+    selector: 'ngx-bootstrap-multiselect',
+    template: `
   <div class="dropdown" (click)="handleComponentClick($event)" >
     <button style="text-align: right"
       [disabled]="disabled"
@@ -33,144 +33,144 @@ export const DROPDOWN_CONTROL_VALUE_ACCESSOR: any = {
     </div>
   </div>
   `,
-  styles: ['.pointer > .dropdown-item { curson: pointer; }'],
-  providers: [DROPDOWN_CONTROL_VALUE_ACCESSOR]
+    styles: ['.pointer > .dropdown-item { curson: pointer; }'],
+    providers: [DROPDOWN_CONTROL_VALUE_ACCESSOR]
 })
 export class NgxBootstrapMultiselectDropdownComponent implements OnInit, ControlValueAccessor {
-  @Input() public disabled: boolean;
-  @Input() public items: any[] = [];
-  @Input() public settings: any;
-  @Output() onDataSelect: EventEmitter<any> = new EventEmitter<any>();
-  @Output() onSelectAllData = new EventEmitter();
-  @Output() onDeselectAllData = new EventEmitter();
-  public innerSettings: DropdownSettings;
-  public selectedText: string;
-  public filteredItems: any[] = [];
-  public filterValue: string = '';
-  public isVisible = false;
-  public selectedItems: any[] = []; // Used to keep track if items currently selected. Dropdown item ids are stored
+    @Input() public disabled: boolean;
+    @Input() public items: any[] = [];
+    @Input() public settings: any;
+    @Output() onDataSelect: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onSelectAllData = new EventEmitter();
+    @Output() onDeselectAllData = new EventEmitter();
+    public innerSettings: DropdownSettings;
+    public selectedText: string;
+    public filteredItems: any[] = [];
+    public filterValue: string = '';
+    public isVisible = false;
+    public selectedItems: any[] = []; // Used to keep track if items currently selected. Dropdown item ids are stored
 
-  // Close dropdown when clicking outside component
-  @HostListener('document:click') clickOutsideComponent() {
-    if(this.isVisible) this.isVisible = false;
-  }
-
-  // Prevent event reaching clickOutsideComponent 
-  handleComponentClick(event: Event) {
-    event.stopPropagation(); 
-  }
-
-  // Return selected items by filtering items array based on values in selectedItems array
-  getSelectedItems() { 
-    this.setSelectedText();
-    return this.items
-      .filter(_ => this.selectedItems.findIndex(x => x === _[this.innerSettings.dataIdProperty]) > -1) // Return only items with id values in selectedItems
-      .slice();
-  }
-
-  // Set text when selecting item from dropdown
-  setSelectedText() {
-    this.selectedText = this.selectedItems.length 
-      ? `${this.selectedItems.length} item${this.selectedItems.length > 1 ? 's' : ''} selected` 
-      : this.innerSettings.noneSelectedBtnText;
-  }
-
-  // Toggle dropdown visibility
-  showDropdown() {
-    this.isVisible = this.isVisible ? false : true;
-  }
-
-  // Filter items based on item name property value
-  onFilterSearch(value: string) {
-    this.filterValue = value; // Save filter value so it appears when toggling dropdown
-    this.filteredItems = this.items.filter(_ => _[this.innerSettings.dataNameProperty] && _[this.innerSettings.dataNameProperty].toLowerCase().startsWith(value));
-  }
-
-  // Set selected dropdown item as active. Activated on dropdown item click
-  onSelect(selectedObject: any) {
-    if(this.disabled) return;
-
-    this.onTouched();
-    this.writeValue(selectedObject);    
-    this.onSelectAllData.emit(selectedObject);
-  }
-
-  // Set all dropdown items as active. Activated on select all button item click
-  onSelectAll() {
-    if(this.disabled) return;
-
-    this.onTouched();
-    this.selectedItems = this.items.map(_ => _[this.innerSettings.dataIdProperty]);
-    this.writeValue(this.selectedItems);    
-    this.onDataSelect.emit();
-  }
-
-  // Remove active from all dropdown items. Activated on deselect all button item click
-  onDeselectAll() {
-    if(this.disabled) return;
-
-    this.onTouched();
-    this.selectedItems = [];
-    this.writeValue([]);    
-    this.onDeselectAllData.emit();
-  }
-
-  // Check if number of selected items is equal or greater than limit
-  isSelectionLimitReached() {
-    return this.innerSettings.selectionLimit && this.innerSettings.selectionLimit <= this.selectedItems.length;
-  }
-
-  // Check if drowdown item is active
-  isActive(item: any) {
-    return this.selectedItems.findIndex(x => x === item[this.innerSettings.dataIdProperty]) > -1;
-  }
-  
-  // Check if input values exist in selectedItems array. If item exists remove from array, else add
-  writeValue(selectedObject: any) {   
-    if(selectedObject) {   
-      const tempArray = Array.isArray(selectedObject) ? selectedObject as any[] : [selectedObject];
-
-      if(tempArray.length === 0) {
-        this.selectedItems = [];
-      }
-      else {
-        this.items = this.items.map(_ => {
-          var index = tempArray.findIndex(x => _[this.innerSettings.dataIdProperty] === x[this.innerSettings.dataIdProperty]);
-          if(index > -1) { 
-            const index = this.selectedItems.findIndex(x => x === _[this.innerSettings.dataIdProperty]);
-            if(index > -1) { this.selectedItems.splice(index, 1); }
-            else {
-              if(!this.isSelectionLimitReached()) {
-                this.selectedItems.push(_[this.innerSettings.dataIdProperty]); 
-              }  
-            }
-          }
-  
-          return _;
-        }).slice();     
-      }     
+    // Close dropdown when clicking outside component
+    @HostListener('document:click') clickOutsideComponent() {
+        if (this.isVisible) this.isVisible = false;
     }
-    this.onChange(this.getSelectedItems());
-  }
 
-  ngOnInit() { 
-    this.innerSettings = new DropdownSettings(this.settings); // Set initial setting values
-    this.filteredItems = this.items; // Set initial filtered values
-    this.setSelectedText(); // Set initial button text
-    console.log(this);
-  }
+    // Prevent event reaching clickOutsideComponent 
+    handleComponentClick(event: Event) {
+        event.stopPropagation();
+    }
 
-  // ControlValueAccessor methods
-  onChange = (selectedObject: any[]) => {};
-  onTouched = () => {};
-  setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-  }
-  registerOnChange(fn: any) {
-    this.onChange = fn;
-  }
-  registerOnTouched(fn: any) {
-    this.onTouched = fn;
-  } 
+    // Return selected items by filtering items array based on values in selectedItems array
+    getSelectedItems() {
+        this.setSelectedText();
+        return this.items
+            .filter(_ => this.selectedItems.findIndex(x => x === _[this.innerSettings.dataIdProperty]) > -1) // Return only items with id values in selectedItems
+            .slice();
+    }
+
+    // Set text when selecting item from dropdown
+    setSelectedText() {
+        this.selectedText = this.selectedItems.length
+            ? `${this.selectedItems.length} ${this.selectedItems.length > 1 ? this.innerSettings.itemSingluarText : this.innerSettings.itemPluralText} selected`
+            : this.innerSettings.noneSelectedBtnText;
+    }
+
+    // Toggle dropdown visibility
+    showDropdown() {
+        this.isVisible = this.isVisible ? false : true;
+    }
+
+    // Filter items based on item name property value
+    onFilterSearch(value: string) {
+        this.filterValue = value; // Save filter value so it appears when toggling dropdown
+        this.filteredItems = this.items.filter(_ => _[this.innerSettings.dataNameProperty] && _[this.innerSettings.dataNameProperty].toLowerCase().startsWith(value));
+    }
+
+    // Set selected dropdown item as active. Activated on dropdown item click
+    onSelect(selectedObject: any) {
+        if (this.disabled) return;
+
+        this.onTouched();
+        this.writeValue(selectedObject);
+        this.onSelectAllData.emit(selectedObject);
+    }
+
+    // Set all dropdown items as active. Activated on select all button item click
+    onSelectAll() {
+        if (this.disabled) return;
+
+        this.onTouched();
+        this.selectedItems = this.items.map(_ => _[this.innerSettings.dataIdProperty]);
+        this.writeValue(this.selectedItems);
+        this.onDataSelect.emit();
+    }
+
+    // Remove active from all dropdown items. Activated on deselect all button item click
+    onDeselectAll() {
+        if (this.disabled) return;
+
+        this.onTouched();
+        this.selectedItems = [];
+        this.writeValue([]);
+        this.onDeselectAllData.emit();
+    }
+
+    // Check if number of selected items is equal or greater than limit
+    isSelectionLimitReached() {
+        return this.innerSettings.selectionLimit && this.innerSettings.selectionLimit <= this.selectedItems.length;
+    }
+
+    // Check if drowdown item is active
+    isActive(item: any) {
+        return this.selectedItems.findIndex(x => x === item[this.innerSettings.dataIdProperty]) > -1;
+    }
+
+    // Check if input values exist in selectedItems array. If item exists remove from array, else add
+    writeValue(selectedObject: any) {
+        if (selectedObject) {
+            const tempArray = Array.isArray(selectedObject) ? selectedObject as any[] : [selectedObject];
+
+            if (tempArray.length === 0) {
+                this.selectedItems = [];
+            }
+            else {
+                this.items = this.items.map(_ => {
+                    var index = tempArray.findIndex(x => _[this.innerSettings.dataIdProperty] === x[this.innerSettings.dataIdProperty]);
+                    if (index > -1) {
+                        const index = this.selectedItems.findIndex(x => x === _[this.innerSettings.dataIdProperty]);
+                        if (index > -1) { this.selectedItems.splice(index, 1); }
+                        else {
+                            if (!this.isSelectionLimitReached()) {
+                                this.selectedItems.push(_[this.innerSettings.dataIdProperty]);
+                            }
+                        }
+                    }
+
+                    return _;
+                }).slice();
+            }
+        }
+        this.onChange(this.getSelectedItems());
+    }
+
+    ngOnInit() {
+        this.innerSettings = new DropdownSettings(this.settings); // Set initial setting values
+        this.filteredItems = this.items; // Set initial filtered values
+        this.setSelectedText(); // Set initial button text
+        console.log(this);
+    }
+
+    // ControlValueAccessor methods
+    onChange = (selectedObject: any[]) => { };
+    onTouched = () => { };
+    setDisabledState(isDisabled: boolean): void {
+        this.disabled = isDisabled;
+    }
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
+    registerOnTouched(fn: any) {
+        this.onTouched = fn;
+    }
 }
 
